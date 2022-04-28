288aa72f04641749a41a3522dbc03aed0ed0f37c
asd3asdasfgasd asdgasdfasdf <nissocialnetwork@gmail.com>
2022-04-28 16:10:14 +0600

    Complete Desktop Version

--- travel/script/script.js
+++ travel/script/script.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-    $(body).on("scroll", function(){
-        alert(" ");
-        let docOffset = $(this).offset();
-        if(docOffset.top > 100){
-            $('#header').css("background-color","#144182");
+    //Header setting up
+    setHeader();
+    $(window).scroll(setHeader);
+
+    //Date on weather
+    const date = new Date();
+    $('#date').html(date.getDate()+'.'+date.getMonth()+'.'+date.getFullYear());
+
+    //CSS properties for header
+    function setHeader(){
+        if($(window).scrollTop() > 100){
+            $('#header').css({
+                backgroundColor: "#144182",
+                padding: "0"
+            });
+        } else {
+            $('#header').css({
+                background: "transparent",
+                padding: "10px 0 0 0"
+            });
         }
-    });
+    }
 });
