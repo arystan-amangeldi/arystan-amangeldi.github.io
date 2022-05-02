80ef9d830302677762b91ad309c94049606b3513
Arystan <a.amangeldi.web@gmail.com>
2022-05-02 15:25:02 +0600

    Laptop Version & Cross Browsing

--- travel/script/script.js
+++ travel/script/script.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //Header setting up
     setHeader();
     $(window).scroll(setHeader);
 
     //Date on weather
     const date = new Date();
-    $('#date').html(date.getDate()+'.'+date.getMonth()+'.'+date.getFullYear());
+    $('#date').html(date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear());
 
     //CSS properties for header
-    function setHeader(){
-        if($(window).scrollTop() > 100){
+    function setHeader() {
+        if ($(window).scrollTop() > 100) {
             $('#header').css({
                 backgroundColor: "#144182",
                 padding: "0"
